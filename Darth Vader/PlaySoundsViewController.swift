4861f225a7e64fb46f86f48c677bcb774324f68a
Mario Blazek <blazek.mario@gmail.com>
2015-10-10 21:07:12 +0200

    Playing audio with AVAudioPlayer

---
+++ Darth Vader/PlaySoundsViewController.swift
@@ -0,0 +1,67 @@
+//
+//  PlaySoundsViewController.swift
+//  Darth Vader
+//
+//  Created by Marek on 10/10/15.
+//  Copyright © 2015 Marek. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+            let filePathUrl = NSURL.fileURLWithPath(filePath)
+            
+            do {
+                audioPlayer =  try AVAudioPlayer(contentsOfURL: filePathUrl)
+                audioPlayer.enableRate = true
+            } catch {
+                
+            }
+            
+        } else {
+            print("the filePath is empty")
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func playSlow(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+    @IBAction func playFast(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 1.5
+        audioPlayer.currentTime = 0.0
+        audioPlayer.play()
+    }
+
+    @IBAction func stop(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
