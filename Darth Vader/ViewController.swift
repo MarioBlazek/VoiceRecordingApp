a082d683ce8e8be66ee072cc602b56c069249dce
Mario Blazek <blazek.mario@gmail.com>
2015-10-10 15:42:58 +0200

    Finished adding link to another view (step3a)

--- Darth Vader/ViewController.swift
+++ Darth Vader/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     
+    @IBOutlet weak var stopButton: UIButton!
+
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,14 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+    }
 
     @IBAction func recordAction(sender: UIButton) {
         recordingInProgress.hidden = false
+        recordButton.enabled = false
+        stopButton.hidden = false
         print("kifla")
     }
 
     @IBAction func stopRecording(sender: UIButton) {
         recordingInProgress.hidden = true
+        recordButton.enabled = true
+        stopButton.hidden = true
     }
 }
 
