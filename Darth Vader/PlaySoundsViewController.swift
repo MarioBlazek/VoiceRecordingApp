d040e2443e0fd349a69ad6ede3a11637597c136d
Mario Blazek <blazek.mario@gmail.com>
2015-10-11 10:29:04 +0200

    Chipmunk and Darth Vader effects added

--- Darth Vader/PlaySoundsViewController.swift
+++ Darth Vader/PlaySoundsViewController.swift
@@ -14,6 +14,10 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayer: AVAudioPlayer!
     
     var receivedAudio: RecordedAudio!
+    
+    var audioEngine: AVAudioEngine!
+    
+    var audioFile: AVAudioFile!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +38,9 @@ class PlaySoundsViewController: UIViewController {
         
         audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl)
         audioPlayer.enableRate = true
+        
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
         
         // Do any additional setup after loading the view.
     }
@@ -58,6 +65,35 @@ class PlaySoundsViewController: UIViewController {
 
     @IBAction func stop(sender: UIButton) {
         audioPlayer.stop()
+    }
+    
+    @IBAction func playChipmunk(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    @IBAction func playDarthVader(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float) {
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
     }
     /*
     // MARK: - Navigation
